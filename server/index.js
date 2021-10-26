8a3682a7529e771fee7966a11619ae4c02c80c2f
thanhantran1 <thanhantran21@gmail.com>
2021-10-26 18:25:01 +0700

    add function register on server side

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const authRouter = require('./routes/auth.router');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(
+            `mongodb+srv://tranthanhan:1234@blog.0gcwr.mongodb.net/Blog?retryWrites=true&w=majority`,
+			{
+				useCreateIndex: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+			}
+		)
+		
+		console.log('MongoDB connected')
+	} catch (error) {
+		console.log(error.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
+
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
